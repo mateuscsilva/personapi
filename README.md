9570a95a80dba00bee4b23db93d9192d856c1035
mateus.carvalho <mateuscsilva.1@gmail.com>
2021-07-17 17:31:51 -0300

    Cria primeira estrutura de organizacao da api

---
+++ README.md
@@ -0,0 +1,41 @@
+#API REST para gerenciamento de pessoas sendo desenvolvida durante o boocamp da Digitial Innnovation One
+
+:computer:
+
+API REST sendo desenvolvida com:
+
+- Java 16
+- Maven 3.8.1  
+- IntelliJ IDEA Community Edition
+- Controle de versão com GIT
+- Armazenamento no Github   
+- Spring Boot
+- H2 database
+- Heroku
+- Postman para teste de funcionalidades da API
+
+Para executar o projeto pelo terminal, digite
+
+```shell script
+mvn spring-boot:run 
+```
+
+Após executar o comando acima, basta apenas abrir o seguinte endereço e visualizar a execução do projeto:
+
+```
+http://localhost:8080/api/v1/people
+```
+
+### Tarefas já definidas:
+
+- [x] Instalar os programas necessários
+- [x] Configurar o ambiente de desenvolvimento
+- [x] Configurar as dependências em pom.xml
+- [x] Configurar a versão do Java e H2 em application.properties
+- [x] Criar o README.md
+- [x] Definir a organização inicial do projeto 
+- [ ] Criar o repositório no Github
+- [ ] Criar as entidades *Person* e *Phone*
+- [ ] Criar os DTO's
+- [ ] Criar as Exceptions
+- [ ] Criar os Services
---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,35 @@
+package one.digitalinnovation.personapi.controller;
+
+import lombok.AllArgsConstructor;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson  = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+
+    @GetMapping
+    public String teste(){
+        return "teste";
+    }
+
+}
